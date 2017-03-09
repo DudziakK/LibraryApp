a7726719d3b9ebe4ea5ce8825396ecfff9230e98
DudziakK <dudziak.krzysztof91@gmail.com>
2017-03-09 15:45:51 +0100

    first steps

---
+++ src/main/java/library/Library.java
@@ -0,0 +1,7 @@
+package library;
+
+/**
+ * Created by Cptonduty on 2017-03-09.
+ */
+public class Library {
+}
---
+++ src/test/java/LibraryTest.java
@@ -0,0 +1,12 @@
+import org.junit.Test;
+
+/**
+ * Created by Cptonduty on 2017-03-09.
+ */
+public class LibraryTest {
+
+    @Test
+    public void firstStuff(){
+
+    }
+}
